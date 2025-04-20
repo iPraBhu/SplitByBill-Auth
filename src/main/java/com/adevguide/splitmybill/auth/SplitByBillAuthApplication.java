c764ef2320a5cb46816b02210b2eb1853da36a3a
PraBhu <pratik.bhuite@gmail.com>
2025-04-19 20:29:34 -0700

    initial commit

---
+++ src/main/java/com/adevguide/splitmybill/auth/SplitByBillAuthApplication.java
@@ -0,0 +1,13 @@
+package com.adevguide.splitmybill.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitByBillAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitByBillAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adevguide/splitmybill/auth/SplitByBillAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adevguide.splitmybill.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitByBillAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
